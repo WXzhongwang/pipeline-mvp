b383ad54dc6bbe77a16a81026ce80e71be1fd825
zhongshengwang <zhongshengwang@shuwen.com>
2021-12-17 22:49:30 +0800

    1. application starter

---
+++ pipeline-framework/src/main/java/com/rany/ops/framework/App.java
@@ -0,0 +1,52 @@
+package com.rany.ops.framework;
+
+import com.rany.ops.framework.core.constants.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 应用程序框架入口
+ * @author zhongshengwang
+ * @description 应用程序框架入口
+ * @date 2021/12/17 10:42 下午
+ * @email 18668485565@163.com
+ */
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    private static boolean waitFlag = true;
+    private static Object waitObject = new Object();
+
+    public static void main(String[] args) {
+        logger.info("pipeline application is starting...");
+
+        // TODO: 应用程序启动
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("shutdown hook thread is running");
+            // TODO: 清理现场
+            activate();
+        }));
+
+        synchronized (waitObject) {
+            logger.info("pipeline application [{}] is waiting for stopping ...", Constants.SYSTEM_NAME);
+            if (waitFlag) {
+                try {
+                    waitObject.wait();
+                } catch (InterruptedException ignore) {
+                }
+            }
+        }
+        logger.info("pipeline application stop finished...");
+    }
+
+    public static void activate() {
+        synchronized (waitObject) {
+            waitObject.notifyAll();
+            waitFlag = false;
+        }
+    }
+
+}
---
+++ pipeline-framework/src/main/java/com/rany/ops/framework/core/constants/Constants.java
@@ -0,0 +1,13 @@
+package com.rany.ops.framework.core.constants;
+
+/**
+ * @author zhongshengwang
+ * @description TODO
+ * @date 2021/12/17 10:48 下午
+ * @email 18668485565@163.com
+ */
+
+public final class Constants {
+
+    public static final String SYSTEM_NAME = "pipeline";
+}
