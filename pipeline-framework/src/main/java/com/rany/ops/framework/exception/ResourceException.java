05f4405fb83f0d5226d48b621e35cd41991a07c9
zhongshengwang <zhongshengwang@shuwen.com>
2021-12-20 22:43:02 +0800

    资源异常

--- pipeline-framework/src/main/java/com/rany/ops/framework/exception/ResourceException.java
+++ pipeline-framework/src/main/java/com/rany/ops/framework/exception/ResourceException.java
@@ -4,7 +4,7 @@ package com.rany.ops.framework.exception;
  * 资源管理统一异常
  *
  * @author zhongshengwang
- * @description TODO
+ * @description 资源管理统一异常
  * @date 2021/12/19 9:21 下午
  * @email 18668485565@163.com
  */
